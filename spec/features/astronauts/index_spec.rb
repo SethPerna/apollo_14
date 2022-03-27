576215b166b9450f364dbabac65b458e887fb260
Seth Perna <gseth26@gmail.com>
2022-03-27 13:05:36 -0400

    complete user story 2

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
-# User Story 1 of 4
-#
-# As a visitor,
-# When I visit '/astronauts'
-# I see a list of astronauts with the following info:
-# - Name
-# - Age
-# - Job
-#
-# (e.g. "Name: Neil Armstrong, Age: 37, Job: Commander")
+
 RSpec.describe "astronauts index page" do
   it "I see a list of astronauts with name age and job" do
     neil = Astronaut.create!(name: 'neil', age: 1, job: 'pilot')
@@ -25,5 +16,20 @@ RSpec.describe "astronauts index page" do
     expect(page).to have_content("Name: #{buzz.name}")
     expect(page).to have_content("Age: #{buzz.age}")
     expect(page).to have_content("Job: #{buzz.job}")
+  end
+#   User Story 2 of 4
+#
+# As a visitor,
+# When I visit '/astronauts'
+# I see the average age of all astronauts.
+#
+# (e.g. "Average Age: 34")
+  it 'had average age of all astronauts' do
+    neil = Astronaut.create!(name: 'neil', age: 1, job: 'pilot')
+    bob = Astronaut.create!(name: 'bob', age: 2, job: 'commander')
+    buzz = Astronaut.create!(name: 'buzz', age: 3, job: 'co-pilot')
+    visit '/astronauts'
+
+    expect(page).to have_content("Average Age: #{Astronaut.average_age}")
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,14 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many(:missions).through(:astronaut_missions) }
   end
+
+  describe 'instance and class methods' do
+    it '.average_age' do
+      neil = Astronaut.create!(name: 'neil', age: 1, job: 'pilot')
+      bob = Astronaut.create!(name: 'bob', age: 2, job: 'commander')
+      buzz = Astronaut.create!(name: 'buzz', age: 3, job: 'co-pilot')
+
+      expect(Astronaut.average_age).to eq(2)
+    end
+  end
 end
